43da0fb59a7f06eb547207684e38236da28413c3
jonathan karlsson <jonathan.s.karlsson@gmail.com>
2013-02-07 08:01:19 +0100

    one can look around with z,x

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #ifdef __APPLE__
 	#include <OpenGL/gl3.h>
 	#include "MicroGlut.h"
-	// linking hint for Lightweight IDE
-	//uses framework Cocoa
 #endif
+#import <ApplicationServices/ApplicationServices.h>
 #include "GL_utilities.h"
 #include "LoadTGA.h"
 #include <math.h>
 #include "loadobj.h"
 #include "VectorUtils2.h"
+#include <stdio.h>
 
-
+/*#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+*/
 
 /* Globals*/
-#define PI 3.14159
 
 GLfloat rot[16], trans[16], shear[16], total[16], cam[16];
 
@@ -28,12 +30,10 @@ GLfloat projMatrix[] = {	2.0f*near/(right-left), 0.0f, (right+left)/(right-left)
 							0.0f, 0.0f, -(far + near)/(far - near), -2*far*near/(far - near),
 							0.0f, 0.0f, -1.0f, 0.0f };
 
-Point3D p,l;
-GLuint program;
-GLuint programShadow;
-GLuint bunnyTex;
-GLuint klingonTex;
-GLuint groundTex;
+GLfloat xCamPos;
+GLfloat zCamPos;
+GLfloat xCamMod;
+GLfloat zCamMod;
 GLfloat xModify;
 GLfloat xValue;
 GLfloat yModify;
@@ -41,12 +41,26 @@ GLfloat yValue;
 GLfloat zModify;
 GLfloat zValue;
 float gravity;
+float angle;
+float angleMod;
 float rotate;
+bool menuPressed;
+
+Point3D p,l;
+GLuint program;
+GLuint programShadow;
+
+GLuint bunnyTex;
+GLuint klingonTex;
+GLuint groundTex;
+GLuint skyBoxTex;
+
 Model *bunny;
 Model *bunnyShadow;
 Model *klingon;
 Model *klingonShadow;
 Model *ground;
+Model *skyBox;
 
 
 
@@ -71,6 +85,10 @@ void init(void) {
 	zValue = -2.0;
 	gravity = 0.0;
 	rotate = 0.0;
+	angle = 0.0;
+	xCamPos = 0.0;
+	zCamPos = 3.0;
+	menuPressed = false;
 
  	/* Load and compile shader*/
 	program = loadShaders("main.vert", "main.frag");
@@ -82,8 +100,10 @@ void init(void) {
 	bunny = LoadModelPlus("bunnyplus.obj");
     klingon = LoadModelPlus("teapot.obj");
     ground = LoadModelPlus("cubeplus.obj");
+    skyBox = LoadModelPlus("skybox.obj");
 
 	LoadTGATextureSimple("maskros512.tga", &bunnyTex);
+	LoadTGATextureSimple("skyBox512.tga", &skyBoxTex);
 	LoadTGATextureSimple("dirt.tga", &klingonTex);
 	LoadTGATextureSimple("grass.tga", &groundTex);
 
@@ -100,8 +120,21 @@ void displayModels(GLfloat t) {
 	glUseProgram(program);
 	glUniformMatrix4fv(glGetUniformLocation(program, "camMatrix"), 1, GL_TRUE, cam);
 
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
 
+	T(0, 0, 0, trans);
+	glBindTexture(GL_TEXTURE_2D, skyBoxTex);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, trans);
+    DrawModel(skyBox, program, "inPosition", "inNormal", "inTexCoord");
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+
+
+	Ry(angle, rot);
 	T(xValue, yValue, zValue, trans);
+    Mult(rot, trans, total);
 	Ry(rotate, rot);
     Mult(trans, rot, total);
 	glBindTexture(GL_TEXTURE_2D, bunnyTex);
@@ -158,21 +191,23 @@ void display(void) {
 	/* clear the screen*/
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
-/*	if (rotateFront == 2) {
-	 	SetVector(xValue, 0.0, zValue-3, &p);
-	 	SetVector(xValue, 0.0, zValue, &l);
-	} else {
-	 	SetVector(xValue, 0.0, (zValue+3), &p);
-	 	SetVector(xValue, 0.0, zValue, &l);
-	}*/	
-	SetVector(xValue, 0.0, (zValue+3), &p);
- 	SetVector(xValue, 0.0, zValue, &l);
+	GLfloat t = (GLfloat)glutGet(GLUT_ELAPSED_TIME);
+
+	angle += angleMod;
+//	xCamPos = xValue + zValue * cos(angle);
+//	zCamPos = zValue + xValue * sin(angle);
+
+	xValue = (xValue + xModify);
+	yValue = yModify;
+	zValue = (zValue + zModify);
+xCamPos += xCamMod;
+zCamPos += zCamMod;
+//	SetVector(xCamPos, 1.0, zCamPos, &p);
+	SetVector(xValue + 3 * cos(xCamPos), 0.0, zValue + 3 * sin(zCamPos), &p);
+	SetVector(xValue, 0.0, zValue, &l);
+
 	lookAt(&p, &l, 0.0, 1.0, 0.0, &cam);
 
-	GLfloat t = (GLfloat)glutGet(GLUT_ELAPSED_TIME);
-	xValue += xModify;
-	yValue += yModify;
-	zValue += zModify;
 
 	displayModels(t);
 	displayShadows(t);
@@ -183,53 +218,102 @@ void display(void) {
 }
 
 void MouseController(int x, int y){
-//	rotate = 1/cos(x);
+	//CGDisplayMoveCursorToPoint(CGDirectDisplayID display, CGPoint point);
+//	CGDisplayMoveCursorToPoint(200,200);
+	if (!menuPressed) {
+		/*GLenum WinX = glutGet(GLUT_WINDOW_WIDTH);
+		GLenum WinY = glutGet(GLUT_WINDOW_HEIGHT);
+
+		CGWarpMouseCursorPosition(CGPointMake(glutGet(GLUT_INIT_WINDOW_X) + WinX / 2, glutGet(GLUT_INIT_WINDOW_Y) + WinY / 2));
+		if (WinX / 2 < x) {
+			xLookAt -= x / WinX;
+		} else {
+			xLookAt += x / WinX;
+		} 
+
+		if (WinY / 2 < y) {
+			yLookAt -= x / WinY;
+		} else {
+			yLookAt += x / WinY;
+		} */
+
+/*		CGWarpMouseCursorPosition(CGPointMake(200,600));
+		if (200 < x) {
+			xLookAt -= x*100;
+		} else {
+			xLookAt += x*100;
+		} 
+
+		if (200 < y) {
+			yLookAt -= y*100;
+		} else {
+			yLookAt += y*100;
+		} */
+	}
+//	SetCursorPos(200,200);
 }
 
 void OnTimer(int value) {
 	xModify = 0.0;
 	zModify = 0.0;
+	angleMod = 0.0;
+	xCamMod = 0.0;
+	zCamMod = 0.0;
 	rotate = 0.0;
 	
 	if (keyIsDown('w') && keyIsDown('d')){
 		xModify = 0.08;
-		rotate = - 3 * PI / 4;
+		rotate = - 3 * M_PI / 4;
 		zModify = -0.08;
 	} else if (keyIsDown('w') && keyIsDown('a')){
 		xModify = -0.08;
-		rotate = 3 * PI / 4;
+		rotate = 3 * M_PI / 4;
 		zModify = -0.08;
 	} else if (keyIsDown('s') && keyIsDown('d')){
 		xModify = 0.08;
-		rotate = - PI / 4;
+		rotate = - M_PI / 4;
 		zModify = 0.08;
 	} else if (keyIsDown('s') && keyIsDown('a')){
 		xModify = -0.08;
-		rotate = PI / 4;
+		rotate = M_PI / 4;
 		zModify = 0.08;
 	} else if (keyIsDown('w')){
-		rotate = PI;
+		rotate = M_PI;
 		zModify = -0.08;
 	} else if (keyIsDown('s')){
 		rotate = 0.0;
 		zModify = 0.08;
 	} else if (keyIsDown('a')){
 		xModify = -0.08;
-		rotate = PI / 2;
+		rotate = M_PI / 2;
 	} else if (keyIsDown('d')){
 		xModify = 0.08;
-		rotate = - PI / 2;
+		rotate = - M_PI / 2;
+	} 
+
+	if (keyIsDown('x')) {
+		angleMod = 0.08;
+		xCamMod = 0.08;
+		zCamMod = 0.08;
+	} else if (keyIsDown('z')) {
+		angleMod = -0.08;
+		xCamMod = -0.08;
+		zCamMod = -0.08;
+	}
+
+	if (keyIsDown('m')) {
+		menuPressed = !menuPressed;
 	}
 
 	if (keyIsDown(' ') && yValue == 0) { 
-		gravity = -0.1;
+		gravity = -0.3;
 		yModify = 0.0;
 		yValue = 0.1;
 	}
 
 	if (gravity < 0 && yValue > 0) {
 		yModify -= gravity;
-		gravity += 0.035;
+		gravity += 0.055;
 	} else if (yValue >= 0.05) {
 		gravity += 0.01;
 		yModify -= gravity;
@@ -244,11 +328,14 @@ void OnTimer(int value) {
 }
 
 int main(int argc, char *argv[]) {
+	glutInitWindowPosition (100, 100);
+	glutInitWindowSize (800, 640);
+
 	glutInit(&argc, argv);
-	glutCreateWindow ("GL3 white triangle example");
+	glutCreateWindow ("Zelda - Quest of the world");
 	glutDisplayFunc(display);
 	initKeymapManager();
-	glutPassiveMotionFunc(MouseController);
+//	glutPassiveMotionFunc(MouseController);
 	glutTimerFunc(20, &OnTimer, 0);
 	init ();
 	glutMainLoop();
